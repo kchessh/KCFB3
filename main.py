b65e238c5b03e5e4612e6a40f0b7d712e4fe5055
Ken Chesshir <kchessh@gmail.com>
2026-07-16 09:17:04 -0500

    Updated main.py to add print statements

--- main.py
+++ main.py
@@ -1858,6 +1858,8 @@ def draft_room(league_id):
     available_teams = [{"name": football_team.team, "id": football_team.id} for football_team in all_football_teams if football_team.id not in teams_to_remove]
     available_teams = sorted(available_teams, key=lambda team: team["name"].lower())
     print(f'{available_teams=}')
+    print(f'{current_winner_name=}')
+    print(f'{nominated_team_name=}')
 
     return render_template('draft/room.html', room=room, participant=participant, active_nomination=active_nomination, participants=participants, available_teams=available_teams,
                            current_winner_name=current_winner_name, nominated_team_name=nominated_team_name)
