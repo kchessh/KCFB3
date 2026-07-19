bc37586a286c4e86b9d18b6b7bc0814fe7e60bd0
Ken Chesshir <kchessh@gmail.com>
2026-07-18 21:36:37 -0500

    Updating main.py for postgres vs postgresql issue (again)

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ load_dotenv()
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith('postgres://'):
     database_url = database_url.replace('postgres://', 'postgresql://', 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "pool_pre_ping": True,
